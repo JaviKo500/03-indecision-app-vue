c819d3a099206680cbff299a4dcea51c81a01f48
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-20 22:48:07 -0500

    delete code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
    <h1 class="text-3xl font-bold underline">
-      My first vue app
+      Hello Vue
    </h1>
-   <hr>
-   <MyCounter :value="5" text="init value 5"/>  
-   <MyCounterScript :value="1" text="init value 1"/>
-   <MyCounterScript2 :value="3" text="init value 3"/>
+ 
 </template>
 <script lang="ts" setup>
-   import MyCounter from './components/MyCounter.vue';
-   import MyCounterScript from './components/MyCounterScript.vue';
-   import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
+ 
 </script>
