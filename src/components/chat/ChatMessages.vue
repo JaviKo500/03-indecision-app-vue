941b1768fb1654d07812c5d3110a96b776c0086c
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-23 22:46:48 -0500

    scroll with new messages

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="flex-1 overflow-y-auto p-4">
+   <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
       <div class="flex flex-col space-y-2">
          <!-- Messages go here -->
          <!-- Example Message -->
@@ -12,10 +12,25 @@
 <script setup lang="ts">
 import type { ChatMessage } from '@/interfaces/chat-message.interface';
 import ChatBubble from './ChatBubble.vue';
+import { ref, watch } from 'vue';
+import { sleep } from '../../helpers/sleep';
 
 interface Props {
    messages: ChatMessage[];
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const chatRef = ref<HTMLDivElement | null>(null);
+
+watch(props.messages, async () => {
+   console.log('<--------------- JK ChatMessages --------------->');
+   console.log(props.messages.length);
+   await sleep(0.5);
+   chatRef.value?.scrollTo({
+      top: chatRef.value?.scrollHeight,
+      behavior: 'smooth',
+   });
+});
+
 </script>
