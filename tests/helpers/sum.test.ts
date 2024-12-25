b382e9003d650e3bde1ac102f757e07aec68be64
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-24 22:35:46 -0500

    test sum array elements

--- tests/helpers/sum.test.ts
+++ tests/helpers/sum.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { sum } from '../../src/helpers/sum';
+import { addArray, sum } from '../../src/helpers/sum';
 
 test('adds 1 + 2 to equal 3', () => {
    // Preparation
@@ -11,4 +11,30 @@ test('adds 1 + 2 to equal 3', () => {
    const result = sum(a, b);
 
    expect(result).toBe(3);
+});
+
+test('should return 15 if the array has 5 elements', () => {
+
+   // Preparation
+   const arr = [1, 2, 3, 4, 5];
+
+   // Execution
+   const result = addArray(arr);
+
+   expect(result).toBe(15);
+});
+
+test('should return 0 if the array is empty', () => {
+   // Preparation
+   const arr = [];
+
+   // Execution
+   const result = addArray(arr);
+
+   expect(result).toBe(0);
+
+});
+
+test('check', () => {
+
 });
