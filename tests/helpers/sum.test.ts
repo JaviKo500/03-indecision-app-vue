2308d55c78abf9859eebd262a2906afa41f366ce
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-24 22:38:04 -0500

    add describe

--- tests/helpers/sum.test.ts
+++ tests/helpers/sum.test.ts
@@ -1,40 +1,39 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { addArray, sum } from '../../src/helpers/sum';
+describe('add function', () => {
+   test('adds 1 + 2 to equal 3', () => {
+      // Preparation
 
-test('adds 1 + 2 to equal 3', () => {
-   // Preparation
+      const a = 1;
+      const b = 2;
 
-   const a = 1;
-   const b = 2;
+      // Execution
+      const result = sum(a, b);
 
-   // Execution
-   const result = sum(a, b);
-
-   expect(result).toBe(3);
+      expect(result).toBe(3);
+   });
 });
 
-test('should return 15 if the array has 5 elements', () => {
+describe('addArray function', () => {
+   test('should return 15 if the array has 5 elements', () => {
 
-   // Preparation
-   const arr = [1, 2, 3, 4, 5];
+      // Preparation
+      const arr = [1, 2, 3, 4, 5];
 
-   // Execution
-   const result = addArray(arr);
+      // Execution
+      const result = addArray(arr);
 
-   expect(result).toBe(15);
-});
+      expect(result).toBe(15);
+   });
 
-test('should return 0 if the array is empty', () => {
-   // Preparation
-   const arr = [];
+   test('should return 0 if the array is empty', () => {
+      // Preparation
+      const arr = [];
 
-   // Execution
-   const result = addArray(arr);
+      // Execution
+      const result = addArray(arr);
 
-   expect(result).toBe(0);
+      expect(result).toBe(0);
 
-});
-
-test('check', () => {
-
+   });
 });
