57e4f5862d83c68d8d92e6e2cb6f0b250e4ab5f2
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-26 23:08:27 -0500

    test key press and empty messages

--- tests/components/chat/MessageBox.test.ts
+++ tests/components/chat/MessageBox.test.ts
@@ -10,6 +10,7 @@ describe('MessageBox.test', () => {
       expect(wrapper.find('button').exists()).toBeTruthy();
       expect(wrapper.find('button svg').exists()).toBe(true);
    });
+
    test('emits message event when button is clicked with message value', async () => {
       const message = 'Hello World';
 
@@ -17,11 +18,29 @@ describe('MessageBox.test', () => {
 
       await wrapper.find('button').trigger('click');
 
-      console.log('<--------------- JK MessageBox.test --------------->');
-      console.log(wrapper.emitted('sendMessage')?.[0]);
-
       expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
 
       expect(wrapper.vm.message).toBe('');
    });
+
+   test('emits message event when keypress.enter is triggered with message value', async () => {
+      const message = 'Hello World';
+
+      const input = await wrapper.find('input[type="text"]');
+
+      await input.setValue(message);
+
+      await input.trigger('keypress.enter');
+
+      expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
+   });
+
+   test('Validation emits message event when keypress.enter or button is triggered with empty message', async () => {
+      const wrapper = mount(MessageBox);
+      const input = wrapper.find('input[type="text"]');
+      await input.trigger('keypress.enter');
+      await wrapper.find('button').trigger('click');
+      expect(wrapper.emitted('sendMessage')).toBeFalsy();
+   });
+
 });
