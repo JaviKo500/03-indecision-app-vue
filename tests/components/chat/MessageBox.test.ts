0cf553f8276a685b1e5f36dac2479712a44bc108
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-26 22:58:30 -0500

    test emit message

--- tests/components/chat/MessageBox.test.ts
+++ tests/components/chat/MessageBox.test.ts
@@ -3,11 +3,25 @@ import { describe, expect, test } from 'vitest';
 import MessageBox from '@/components/chat/MessageBox.vue';
 
 describe('MessageBox.test', () => {
+   const wrapper = mount(MessageBox);
    test('render input and button elements correctly', () => {
-      const wrapper = mount(MessageBox);
       expect(wrapper.html()).toMatchSnapshot();
       expect(wrapper.find('input[type="text"]').exists()).toBeTruthy();
       expect(wrapper.find('button').exists()).toBeTruthy();
       expect(wrapper.find('button svg').exists()).toBe(true);
+   });
+   test('emits message event when button is clicked with message value', async () => {
+      const message = 'Hello World';
+
+      await wrapper.find('input[type="text"]').setValue(message);
+
+      await wrapper.find('button').trigger('click');
+
+      console.log('<--------------- JK MessageBox.test --------------->');
+      console.log(wrapper.emitted('sendMessage')?.[0]);
+
+      expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
+
+      expect(wrapper.vm.message).toBe('');
    });
 });
